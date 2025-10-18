cb0408e6bf7583576763f98753e97808963ca282
reginabeltran06 <reginabeltran06@gmail.com>
2025-10-18 00:03:54 -0600

    changes for render

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import axios from "axios";
 
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
